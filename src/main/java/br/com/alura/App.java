46f45a389373be9e0b82db20cf61ee7800c0c940
Thiago Cavalcante <thiagocavalcante@zup.com.br>
2022-03-18 14:01:09 -0300

    Add new fields to products
    Changes:
    - New fields: creation date (LocalDate) and category (custom)
    - New enum for categories with three values
    - New product constructor

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -5,17 +5,16 @@ import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 
 import br.com.alura.dao.ProdutoDao;
+import br.com.alura.modelo.Categoria;
 import br.com.alura.modelo.Produto;
 import br.com.alura.util.JPAUtil;
 
 public class App {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Um celular.");
-        celular.setPreco(new BigDecimal("800"));
+        Produto celular = new Produto(
+            "Xiaomi Redmi", "Um celular.", new BigDecimal("800"), Categoria.CELULARES
+        );
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao dao = new ProdutoDao(em);
---
+++ src/main/java/br/com/alura/modelo/Categoria.java
@@ -0,0 +1,5 @@
+package br.com.alura.modelo;
+
+public enum Categoria {
+    CELULARES, INFORMATICA, LIVROS;
+}
