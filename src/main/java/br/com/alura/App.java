129eddca74c25c04aa64475ff02cbc2d0909fd60
Thiago Cavalcante <thiagocavalcante@zup.com.br>
2022-03-18 13:07:29 -0300

    Add code to store a product in the database
    Other changes:
    - Fix persistence.xml persistence tag attributes
    - New persistence.xml config to show SQL statements
    - New persistence.xml config to automatically update the DB

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -1,9 +1,29 @@
 package br.com.alura;
 
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.modelo.Produto;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi Redmi");
+        celular.setDescricao("Um celular.");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory(
+            "alura-persistencia-jpa"
+        );
+        EntityManager em = emFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
     }
 
 }
